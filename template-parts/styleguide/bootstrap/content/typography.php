554b48cd89a3f11f7102eb0095b6309af689eef5
WPGuru4u <wpguru4u@gmail.com>
2017-09-21 22:22:15 -0400

    template with a bunch of typography examples

---
+++ template-parts/styleguide/bootstrap/content/typography.php
@@ -0,0 +1,200 @@
+<?php
+?>
+<h2>Typography</h2>
+<p class="lead">Headings, body text, lists, and more</p>
+
+<h3>Headings</h3>
+<h1>h1 Bootstrap heading</h1>
+<h2>h2 Bootstrap heading</h2>
+<h3>h3 Bootstrap heading</h3>
+<h4>h4 Bootstrap heading</h4>
+<h5>h5 Bootstrap heading</h5>
+<h6>h6 Bootstrap heading</h6>
+
+<p class="h1">.h1 Bootstrap heading</p>
+<p class="h2">.h2 Bootstrap heading</p>
+<p class="h3">.h3 Bootstrap heading</p>
+<p class="h4">.h4 Bootstrap heading</p>
+<p class="h5">.h5 Bootstrap heading</p>
+<p class="h6">.h6 Bootstrap heading</p>
+
+<div class="ws"></div>
+
+<h3>Display headings</h3>
+<h1 class="display-1">Display 1</h1>
+<h1 class="display-2">Display 2</h1>
+<h1 class="display-3">Display 3</h1>
+<h1 class="display-4">Display 4</h1>
+
+<div class="ws"></div>
+
+<h3>Lead</h3>
+<p class="lead">
+  Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus.
+</p>
+
+<div class="ws"></div>
+
+<h3>Inline text elements</h3>
+
+<p>You can use the mark tag to <mark>highlight</mark> text.</p>
+<p><del>This line of text is meant to be treated as deleted text.</del></p>
+<p><s>This line of text is meant to be treated as no longer accurate.</s></p>
+<p>These tests are a <big>big</big> deal, but this tag is no longer supported in HTML5.</p>
+<p><ins>This line of text is meant to be treated as an addition to the document.</ins></p>
+<p><u>This line of text will render as underlined</u></p>
+<p><small>This line of text is meant to be treated as fine print.</small></p>
+<p><strong>This line rendered as bold text.</strong></p>
+<p><em>This line rendered as italicized text.</em></p>
+<p>The abbreviation <abbr title="Seriously">srsly</abbr> stands for "seriously".</p>
+<p>The acronym <acronym title="For The Win">ftw</acronym> stands for "for the win".</p>
+<p><q>Test the q and cite tag.</q> --<cite>Robots</cite></p>
+<p>This will test the <code>&lt;code&gt;</code> tag.</p>
+<p>Getting our science styling on with H<sub>2</sub>O, which should push the "2" down.</p>
+<p>Still sticking with science and Isaac Newton's E = MC<sup>2</sup>, which should lift the 2 up.</p>
+<p>This rarely used tag emulates <tt>teletype text</tt>.</p>
+<p>The var tag is used on the variables of this equation: <var>y</var> = <var>m</var><var>x</var> + <var>b</var></p>
+<p>Use the <kbd>kbd tag</kbd> to indicate input that is typically entered via keyboard.</p>
+<p><samp>This text is meant to be treated as sample output from a computer program.</samp></p>
+This is an example of a <a title="Visit WordPress.org" href="http://wordpress.org">link</a>
+<p>
+<address>123 Test Address<br>
+Cupertino, CA 95014<br>
+United States</address></p>
+
+<div class="ws"></div>
+
+<h3>Block text elements</h3>
+<blockquote>
+  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+  <footer>Someone famous in <cite title="Source Title">Source Title</cite></footer>
+</blockquote>
+<div class="ws"></div>
+
+<blockquote class="blockquote">
+  <p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+</blockquote>
+<div class="ws"></div>
+<blockquote class="blockquote">
+  <p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+  <footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
+</blockquote>
+<div class="ws"></div>
+<blockquote class="blockquote text-center">
+  <p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+  <footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
+</blockquote>
+<div class="ws"></div>
+<blockquote class="blockquote text-right">
+  <p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+  <footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
+</blockquote>
+<div class="ws"></div>
+<pre><code>&lt;p&gt;Sample text here...&lt;/p&gt;
+&lt;p&gt;And another line of sample text here...&lt;/p&gt;
+</code></pre>
+<div class="ws"></div>
+<pre class="pre-scrollable"><code>&lt;p&gt;Sample text here...&lt;/p&gt;
+&lt;p&gt;And another line of sample text here...&lt;/p&gt;
+</code></pre>
+
+<div class="ws"></div>
+
+<h3>Unordered Lists (Nested)</h3>
+<ul>
+	<li>List item one
+<ul>
+	<li>List item one
+<ul>
+	<li>List item one</li>
+	<li>List item two</li>
+	<li>List item three</li>
+	<li>List item four</li>
+</ul>
+</li>
+	<li>List item two</li>
+	<li>List item three</li>
+	<li>List item four</li>
+</ul>
+</li>
+	<li>List item two</li>
+	<li>List item three</li>
+	<li>List item four</li>
+</ul>
+<h3>Ordered List (Nested)</h3>
+<ol>
+	<li>List item one
+<ol>
+	<li>List item one
+<ol>
+	<li>List item one</li>
+	<li>List item two</li>
+	<li>List item three</li>
+	<li>List item four</li>
+</ol>
+</li>
+	<li>List item two</li>
+	<li>List item three</li>
+	<li>List item four</li>
+</ol>
+</li>
+	<li>List item two</li>
+	<li>List item three</li>
+	<li>List item four</li>
+</ol>
+
+<h3>Definition Lists</h3>
+<dl><dt>Definition List Title</dt><dd>Definition list division.</dd><dt>Startup</dt><dd>A startup company or startup is a company or temporary organization designed to search for a repeatable and scalable business model.</dd><dt>#dowork</dt><dd>Coined by Rob Dyrdek and his personal body guard Christopher "Big Black" Boykins, "Do Work" works as a self motivator, to motivating your friends.</dd><dt>Do It Live</dt><dd>I'll let Bill O'Reilly will <a title="We'll Do It Live" href="https://www.youtube.com/watch?v=O_HyZ5aW76c">explain</a> this one.</dd></dl>
+
+<h3>Responsive Definition Lists (alternative)</h3>
+<dl class="row">
+  <dt class="col-sm-3">Description lists</dt>
+  <dd class="col-sm-9">A description list is perfect for defining terms.</dd>
+
+  <dt class="col-sm-3">Euismod</dt>
+  <dd class="col-sm-9">
+    <p>Vestibulum id ligula porta felis euismod semper eget lacinia odio sem nec elit.</p>
+    <p>Donec id elit non mi porta gravida at eget metus.</p>
+  </dd>
+
+  <dt class="col-sm-3">Malesuada porta</dt>
+  <dd class="col-sm-9">Etiam porta sem malesuada magna mollis euismod.</dd>
+
+  <dt class="col-sm-3 text-truncate">Truncated term is truncated</dt>
+  <dd class="col-sm-9">Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</dd>
+
+  <dt class="col-sm-3">Nesting</dt>
+  <dd class="col-sm-9">
+    <dl class="row">
+      <dt class="col-sm-4">Nested definition list</dt>
+      <dd class="col-sm-8">Aenean posuere, tortor sed cursus feugiat, nunc augue blandit nunc.</dd>
+    </dl>
+  </dd>
+</dl>
+
+<h3>Colors</h3>
+<p class="text-primary">.text-primary</p>
+<p class="text-secondary">.text-secondary</p>
+<p class="text-success">.text-success</p>
+<p class="text-danger">.text-danger</p>
+<p class="text-warning">.text-warning</p>
+<p class="text-info">.text-info</p>
+<p class="text-light">.text-light</p>
+<p class="text-dark">.text-dark</p>
+<p><a href="#" class="text-primary">Primary link</a></p>
+<p><a href="#" class="text-secondary">Secondary link</a></p>
+<p><a href="#" class="text-success">Success link</a></p>
+<p><a href="#" class="text-danger">Danger link</a></p>
+<p><a href="#" class="text-warning">Warning link</a></p>
+<p><a href="#" class="text-info">Info link</a></p>
+<p><a href="#" class="text-light bg-gray">Light link</a></p>
+<p><a href="#" class="text-dark">Dark link</a></p>
+<div class="bg-primary text-white">.bg-primary</div>
+<div class="bg-secondary text-white">.bg-secondary</div>
+<div class="bg-success text-white">.bg-success</div>
+<div class="bg-danger text-white">.bg-danger</div>
+<div class="bg-warning text-white">.bg-warning</div>
+<div class="bg-info text-white">.bg-info</div>
+<div class="bg-light text-gray-dark">.bg-light</div>
+<div class="bg-dark text-white">.bg-dark</div>
+<div class="bg-white text-gray-dark">.bg-white</div>
