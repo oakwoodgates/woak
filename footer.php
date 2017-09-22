0cc218e9f3d6ae0aaa381ab2a166f37c92922ca2
WPGuru4u <wpguru4u@gmail.com>
2017-09-21 21:05:47 -0400

    footer socket markup

--- footer.php
+++ footer.php
@@ -13,17 +13,26 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer">
+
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', '_s' ) ); ?>"><?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', '_s' ), 'WordPress' );
-			?></a>
-			<span class="sep"> | </span>
-			<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', '_s' ), '_s', '<a href="https://automattic.com/">Automattic</a>' );
-			?>
+			<div class="container">
+
+			</div>
 		</div><!-- .site-info -->
+		<div class="socket">
+			<div class="container">
+				<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'woak' ) ); ?>"><?php
+					/* translators: %s: CMS name, i.e. WordPress. */
+					printf( esc_html__( 'Proudly powered by %s', 'woak' ), 'WordPress' );
+				?></a>
+				<span class="sep"> | </span>
+				<?php
+					/* translators: 1: Theme name, 2: Theme author. */
+					printf( esc_html__( 'Theme: %1$s by %2$s.', 'woak' ), 'Woak', '<a href="https://github.com/oakwoodgates">OakwoodGates</a>' );
+				?>
+			</div>
+		</div><!-- .site-info -->
+
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
