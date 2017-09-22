e11f1ca1da35520b6eff2c08d912742fab02d3e3
WPGuru4u <wpguru4u@gmail.com>
2017-09-21 21:02:24 -0400

    styleguide template updates

--- page-templates/styleguide.php
+++ page-templates/styleguide.php
@@ -5,34 +5,61 @@
   <?php get_template_part('templates/content', 'page'); ?>
 
   <?php
-    $templates = array(
-      'alerts',
-      'badge',
-      'breadcrumb',
-      'buttons',
-      'button-group',
+    $components = array(
+//      'alerts',
+//      'badge',
+//      'breadcrumb',
+//      'buttons',
+//      'button-group',
       //'card',
-      //'carousel',
-      'collapse',
-      'dropdowns',
+//      'carousel',
+//      'collapse',
+//      'dropdowns',
       //'forms',
       //'input-group',
-      'list-group',
-      'modal',
-      'navs',
-      'navs-js',
-      'navbar',
-      'pagination',
-      //'popovers',
-      'progress',
+//      'jumbotron',
+//      'list-group',
+//      'modal',
+//      'navs',
+//      'navs-js',
+//      'navbar',
+//      'pagination',
+//      'popovers',
+//      'progress',
       //'scrollspy',
-      //'tooltips',
+//      'tooltips',
     );
-    foreach ($templates as $t) {
-      get_template_part('/template-parts/styleguide/bootstrap/'.$t);
+    foreach ($components as $c) {
+      get_template_part('/template-parts/styleguide/bootstrap/'.$c);
+      echo '<hr />';
+    }
+
+    $standards = array(
+//      'typography',
+//      'tables'
+    );
+    foreach ($standards as $s) {
+  //    get_template_part('/template-parts/styleguide/bootstrap/content/'.$s);
+      echo '<hr />';
+    }
+
+    $sections = array(
+      'fs-1',
+    );
+    foreach ($sections as $s) {
+      get_template_part('/template-parts/styleguide/bootstrap/sections/'.$s);
       echo '<hr />';
     }
    ?>
 
 <?php endwhile; ?>
+<script style="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/holder/2.9.4/holder.js"></script>
+<script type="text/javascript">
+jQuery(function () {
+  jQuery('[data-toggle="tooltip"]').tooltip()
+});
+jQuery(function () {
+  jQuery('[data-toggle="popover"]').popover()
+})
+</script>
 <?php get_footer();
