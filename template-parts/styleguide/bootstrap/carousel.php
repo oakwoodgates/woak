13122d130d623e3503019d40ee69a2c644912184
WPGuru4u <wpguru4u@gmail.com>
2017-09-21 20:32:44 -0400

    carousel component

---
+++ template-parts/styleguide/bootstrap/carousel.php
@@ -0,0 +1,135 @@
+<h2>Carousel</h2>
+<p class="lead">A slideshow component for cycling through elements—images or slides of text—like a carousel.</p>
+<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" alt="First slide [800x400]" src="holder.js/800x400?auto=yes&amp;bg=777&amp;fg=555&amp;text=First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" alt="Second slide [800x400]" src="holder.js/800x400?auto=yes&amp;bg=555&amp;fg=333&amp;text=Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" alt="Third slide [800x400]" src="holder.js/800x400?auto=yes&amp;bg=555&amp;fg=333&amp;text=Third slide">
+    </div>
+  </div>
+</div>
+
+<div class="ws"></div>
+
+<h3>With Controls</h3>
+<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" alt="First slide [800x400]" src="holder.js/800x400?auto=yes&amp;bg=777&amp;fg=555&amp;text=First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" alt="Second slide [800x400]" src="holder.js/800x400?auto=yes&amp;bg=777&amp;fg=555&amp;text=Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" alt="Third slide [800x400]" src="holder.js/800x400?auto=yes&amp;bg=777&amp;fg=555&amp;text=Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+<div class="ws"></div>
+
+<h3>With Controls, Indicators, & Captions</h3>
+<div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleCaptions" data-slide-to="1" class=""></li>
+    <li data-target="#carouselExampleCaptions" data-slide-to="2" class=""></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" alt="First slide [800x400]" src="holder.js/800x400?auto=yes&amp;bg=777&amp;fg=222&amp;text=First slide">
+      <div class="carousel-caption d-none d-md-block">
+        <h3>First slide label</h3>
+        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" alt="Second slide [800x400]" src="holder.js/800x400?auto=yes&amp;bg=555&amp;fg=333&amp;text=Second slide">
+      <div class="carousel-caption d-none d-md-block">
+        <h3>Second slide label</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" alt="Third slide [800x400]" src="holder.js/800x400?auto=yes&amp;bg=222&amp;fg=777&amp;text=Third slide">
+      <div class="carousel-caption d-none d-md-block">
+        <h3>Third slide label</h3>
+        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+      </div>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+<div class="ws"></div>
+
+</div>
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1" class=""></li>
+    <li data-target="#myCarousel" data-slide-to="2" class=""></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="holder.js/1800x800?auto=yes&amp;bg=555&amp;fg=555&amp;textmode=exact" alt="First slide">
+      <div class="container">
+        <div class="carousel-caption d-none d-md-block text-left">
+          <h1>Example headline.</h1>
+          <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+          <p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
+        </div>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="holder.js/1800x800?auto=yes&amp;bg=555&amp;fg=555&amp;text=&amp;textmode=exact" alt="Second slide">
+      <div class="container">
+        <div class="carousel-caption d-none d-md-block">
+          <h1>Another example headline.</h1>
+          <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+          <p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
+        </div>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="holder.js/1800x800?auto=yes&amp;bg=555&amp;fg=555&amp;textmode=exact" alt="Third slide">
+      <div class="container">
+        <div class="carousel-caption d-none d-md-block text-right">
+          <h1>One more for good measure.</h1>
+          <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+          <p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+<div class="container">
